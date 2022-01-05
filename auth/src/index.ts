984e8b858e2f5885d0634ce683ac41216302c4c2
JatinBumbra <jatin.bumbra.23@gmail.com>
2022-01-05 23:02:42 +0530

    Segregate auth routes

--- auth/src/index.ts
+++ auth/src/index.ts
@@ -1,11 +1,16 @@
 import express from 'express';
+import { signInRouter } from './routes/signin';
+import { signUpRouter } from './routes/signup';
+import { signOutRouter } from './routes/signout';
+import { currentUserRouter } from './routes/current-user';
 
 const app = express()
 app.use(express.json());
 
-app.get('/api/users/currentuser',(req,res) => {
-    res.send('Hi there!!');
-})
+app.use(currentUserRouter)
+app.use(signInRouter)
+app.use(signOutRouter)
+app.use(signUpRouter)
 
 app.listen(3000, () => {
     console.log('Listening on port 3000')
---
+++ auth/src/routes/signout.ts
@@ -0,0 +1,9 @@
+import express from 'express'
+
+const router = express.Router()
+
+router.get('/api/users/signout',(req,res) => {
+    res.send('/api/users/signout')
+})
+
+export {router as signOutRouter}
