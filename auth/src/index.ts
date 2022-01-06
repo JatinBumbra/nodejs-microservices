c1489396ab341cafe34d1bb9cbdcf127f1ffcf72
JatinBumbra <jatin.bumbra.23@gmail.com>
2022-01-06 13:08:04 +0530

    Issue JWT to signedup user

--- auth/src/index.ts
+++ auth/src/index.ts
@@ -2,6 +2,7 @@ import express from 'express';
 require('express-async-errors');
 
 import mongoose from 'mongoose'
+import cookieSession from 'cookie-session';
 
 import { signInRouter } from './routes/signin';
 import { signUpRouter } from './routes/signup';
@@ -11,7 +12,13 @@ import { errorHandler } from './middlewares/error-handler';
 import { NotFoundError } from './errors/not-found-error';
 
 const app = express()
+app.set('trust proxy',true)
 app.use(express.json());
+
+app.use(cookieSession({
+    signed: false,
+    secure: true
+}))
 
 app.use(currentUserRouter)
 app.use(signInRouter)
@@ -25,6 +32,10 @@ app.all('*',async() => {
 app.use(errorHandler)
 
 const start = async() => {
+    if(!process.env.JWT_KEY) {
+        throw new Error('Missing env var JWT_KEY')
+    }
+
     console.log('Connecting to MongoDB...')
     try {
         await mongoose.connect('mongodb://auth-mongo-service:27017/auth')
