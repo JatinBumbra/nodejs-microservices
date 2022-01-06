bfe9e281798b22b47d05b0e451ee582bef264a49
JatinBumbra <jatin.bumbra.23@gmail.com>
2022-01-06 15:37:59 +0530

    Refactor app. Setup tests and write initial test for signup

---
+++ auth/src/app.ts
@@ -0,0 +1,33 @@
+import express from 'express';
+require('express-async-errors');
+
+import cookieSession from 'cookie-session';
+
+import { signInRouter } from './routes/signin';
+import { signUpRouter } from './routes/signup';
+import { signOutRouter } from './routes/signout';
+import { currentUserRouter } from './routes/current-user';
+import { errorHandler } from './middlewares/error-handler';
+import { NotFoundError } from './errors/not-found-error';
+
+const app = express()
+app.set('trust proxy',true)
+app.use(express.json());
+
+app.use(cookieSession({
+    signed: false,
+    secure: true
+}))
+
+app.use(currentUserRouter)
+app.use(signInRouter)
+app.use(signOutRouter)
+app.use(signUpRouter)
+
+app.all('*',async() => {
+    throw new NotFoundError()
+})
+
+app.use(errorHandler)
+
+export {app}
--- auth/src/index.ts
+++ auth/src/index.ts
@@ -1,35 +1,6 @@
-import express from 'express';
-require('express-async-errors');
-
 import mongoose from 'mongoose'
-import cookieSession from 'cookie-session';
 
-import { signInRouter } from './routes/signin';
-import { signUpRouter } from './routes/signup';
-import { signOutRouter } from './routes/signout';
-import { currentUserRouter } from './routes/current-user';
-import { errorHandler } from './middlewares/error-handler';
-import { NotFoundError } from './errors/not-found-error';
-
-const app = express()
-app.set('trust proxy',true)
-app.use(express.json());
-
-app.use(cookieSession({
-    signed: false,
-    secure: true
-}))
-
-app.use(currentUserRouter)
-app.use(signInRouter)
-app.use(signOutRouter)
-app.use(signUpRouter)
-
-app.all('*',async() => {
-    throw new NotFoundError()
-})
-
-app.use(errorHandler)
+import { app } from './app';
 
 const start = async() => {
     if(!process.env.JWT_KEY) {
