08e9aec200a05ffe31760fa4ee2799a6301ce434
JatinBumbra <jatin.bumbra.23@gmail.com>
2022-01-06 13:27:27 +0530

    Sign in user route handler

--- auth/src/routes/signup.ts
+++ auth/src/routes/signup.ts
@@ -1,9 +1,9 @@
 import express, {Request,Response} from 'express'
-import { body, validationResult } from 'express-validator'
+import { body } from 'express-validator'
 import jwt from 'jsonwebtoken'
 import { BadRequestError } from '../errors/bad-request-error';
-import { RequestValidationError } from '../errors/request-validation-error';
 import { User } from '../models/user';
+import { validateRequest } from '../middlewares/validate-request';
 
 const router = express.Router()
 
@@ -11,13 +11,8 @@ router.post('/api/users/signup',[
         body('email').isEmail().withMessage('Invalid email'),
         body('password').trim().isLength({min:4,max:20}).withMessage('Password must be between 4 to 20 characters'),
     ],
+    validateRequest,
     async (req:Request,res:Response) => {
-        const errors = validationResult(req);
-
-        if(!errors.isEmpty()) {
-            throw new RequestValidationError(errors.array())
-        }
-
         const {email,password} = req.body;
 
         const existingUser = await User.findOne({email})
@@ -33,9 +28,7 @@ router.post('/api/users/signup',[
             jwt: userJwt
         };
 
-        res.status(201).send({
-            user
-        })
+        res.status(201).send(user)
 })
 
 export {router as signUpRouter}
