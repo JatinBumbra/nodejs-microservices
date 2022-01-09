7cbdb47f39177f684438c65f5217a8c41e1a2ad0
JatinBumbra <jatin.bumbra.23@gmail.com>
2022-01-10 01:59:38 +0530

    Update event

--- common/src/events/order-created-event.ts
+++ common/src/events/order-created-event.ts
@@ -1,11 +1,10 @@
-import { OrderStatus } from './types/order-status';
 import { Subjects } from "./subjects";
 
 export interface OrderCreatedEvent {
     subject: Subjects.OrderCreated;
     data: {
         id: string,
-        version: string,
+        version: number,
         expiresAt: string,
         userId: string,
         ticket: {
