a837db2d0e89553e62a9e8d14bdd4a51d509bb8b
JatinBumbra <jatin.bumbra.23@gmail.com>
2022-01-10 10:53:13 +0530

    Change status in OrderCreatedEvent in common module

--- common/src/events/order-cancelled-event.ts
+++ common/src/events/order-cancelled-event.ts
@@ -4,7 +4,7 @@ export interface OrderCancelledEvent {
     subject: Subjects.OrderCancelled;
     data: {
         id: string,
-        status: Subjects,
+        status: string,
         version: number,
         ticket: {
             id: string,
